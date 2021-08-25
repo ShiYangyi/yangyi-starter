070283ea3503fb7e0789425d901702dd79d15bdf
yangyi.shi <syy13983906480@163.com>
2021-08-25 10:06:33 +0800

    [ShiYangyi] add GreetingControllerTest

---
+++ src/test/java/com/example/yangyistarter/controller/GreetingControllerTest.java
@@ -0,0 +1,36 @@
+package com.example.yangyistarter.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class GreetingControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Test
+    public void should_return_string_when_request_greeting_interface() throws Exception {
+
+        MockHttpServletRequestBuilder requestBuilder = get("/hello")
+                .contentType(MediaType.APPLICATION_JSON);
+
+        MockHttpServletResponse response = mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse();
+
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+        assertEquals("hello, world!", response.getContentAsString());
+    }
+}
