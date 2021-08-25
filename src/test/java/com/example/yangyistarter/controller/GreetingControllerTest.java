2adf6161b33e8df15337a5cf99c6ad2281202875
yangyi.shi <syy13983906480@163.com>
2021-08-25 21:52:44 +0800

    [ShiYangyi] modify GreetingControllerTest

--- src/test/java/com/example/yangyistarter/controller/GreetingControllerTest.java
+++ src/test/java/com/example/yangyistarter/controller/GreetingControllerTest.java
@@ -4,17 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@AutoConfigureMockMvc
+/*@AutoConfigureMockMvc
 @SpringBootTest
 public class GreetingControllerTest {
     @Autowired
@@ -33,4 +30,22 @@ public class GreetingControllerTest {
 
         assertEquals(HttpStatus.FORBIDDEN.value(), response.getStatus());
     }
+}*/
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class GreetingControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    //由于所有人都不能访问接口，所以这个测试方法是没有访问到controller层，被WebSecurityConfig所拦截，
+    // 写的这个方法也就不能提高controller层的测试覆盖率，如果有些人可以访问，就可以增加相应测试
+    @Test
+    public void should_refuse_when_request_greeting_interface() throws Exception {
+
+        mockMvc.perform(get("/hello").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError());
+    }
 }
+
