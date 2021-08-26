9e00d0c6a190c2f04e9a43a219d4e0670c294bd3
yangyi.shi <syy13983906480@163.com>
2021-08-26 11:35:30 +0800

    [ShiYangyi] modify WebSecurityConfig

--- src/main/java/com/example/yangyistarter/config/WebSecurityConfig.java
+++ src/main/java/com/example/yangyistarter/config/WebSecurityConfig.java
@@ -1,33 +1,16 @@
 package com.example.yangyistarter.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/hello")
-                .authenticated()
-                .and()
-                .httpBasic();
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        auth.inMemoryAuthentication()
-                .passwordEncoder(encoder)
-                .withUser("admin")
-                .password(encoder.encode("password"))
-                .roles("ADMIN");
+        http.authorizeRequests().anyRequest()
+                .authenticated();
     }
 }
