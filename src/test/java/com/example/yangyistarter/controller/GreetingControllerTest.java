7cb39e41c11e8d4734c8d9acdc40f05534eadd3c
yangyi.shi <syy13983906480@163.com>
2021-08-26 11:07:37 +0800

    [ShiYangyi] add authenticated user test in GreetingControllerTest

--- src/test/java/com/example/yangyistarter/controller/GreetingControllerTest.java
+++ src/test/java/com/example/yangyistarter/controller/GreetingControllerTest.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -39,13 +41,20 @@ public class GreetingControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    //由于所有人都不能访问接口，所以这个测试方法是没有访问到controller层，被WebSecurityConfig所拦截，
-    // 写的这个方法也就不能提高controller层的测试覆盖率，如果有些人可以访问，就可以增加相应测试
     @Test
     public void should_refuse_when_request_greeting_interface() throws Exception {
 
         mockMvc.perform(get("/hello").contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().is4xxClientError());
+    }
+
+    @WithMockUser(username="admin")
+    @Test
+    public void should_return_string_when_access_to_interface() throws Exception {
+
+        MvcResult mvcResult = mockMvc.perform(get("/hello").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk()).andReturn();
+        assertThat(mvcResult.getResponse().getContentAsString()).isEqualTo("hello, world!");
     }
 }
 
