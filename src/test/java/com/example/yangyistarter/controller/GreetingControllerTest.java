e3220da5ff75b99569ae3387f8c0c621d82324d3
ShiYangyi <49311785+ShiYangyi@users.noreply.github.com>
2021-08-26 11:39:48 +0800

    [ShiYangyi] add ci.yml (#12)

--- src/test/java/com/example/yangyistarter/controller/GreetingControllerTest.java
+++ src/test/java/com/example/yangyistarter/controller/GreetingControllerTest.java
@@ -4,33 +4,36 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.MvcResult;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
 @SpringBootTest
 public class GreetingControllerTest {
+
     @Autowired
     private MockMvc mockMvc;
+
     @Test
-    public void should_return_string_when_request_greeting_interface() throws Exception {
+    public void should_refuse_when_request_greeting_interface() throws Exception {
 
-        MockHttpServletRequestBuilder requestBuilder = get("/hello")
-                .contentType(MediaType.APPLICATION_JSON);
+        mockMvc.perform(get("/hello").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError());
+    }
 
-        MockHttpServletResponse response = mockMvc.perform(requestBuilder)
-                .andExpect(status().isOk())
-                .andReturn()
-                .getResponse();
+    @WithMockUser
+    @Test
+    public void should_return_string_when_access_to_interface() throws Exception {
 
-        assertEquals(HttpStatus.OK.value(), response.getStatus());
-        assertEquals("hello, world!", response.getContentAsString());
+        MvcResult mvcResult = mockMvc.perform(get("/hello").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk()).andReturn();
+        assertThat(mvcResult.getResponse().getContentAsString()).isEqualTo("hello, world!");
     }
 }
+
