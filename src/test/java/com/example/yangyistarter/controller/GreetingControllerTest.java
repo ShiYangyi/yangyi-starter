6d9d247ed92b86d948aeeb5cfdc333ad4dcbbf5a
yangyi.shi <syy13983906480@163.com>
2021-08-26 17:21:11 +0800

    [ShiYangyi] add ActiveProfiles annotation in GreetingControllerTest

--- src/test/java/com/example/yangyistarter/controller/GreetingControllerTest.java
+++ src/test/java/com/example/yangyistarter/controller/GreetingControllerTest.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
@@ -13,6 +14,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ActiveProfiles("test")
 @AutoConfigureMockMvc
 @SpringBootTest
 public class GreetingControllerTest {
