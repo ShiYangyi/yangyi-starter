a41b14f2898f514464dfaac3670eb2d04430b9ac
yangyi.shi <syy13983906480@163.com>
2021-08-30 14:07:30 +0800

    [ShiYangyi] add User constructor annotations

--- src/main/java/com/example/yangyistarter/entity/User.java
+++ src/main/java/com/example/yangyistarter/entity/User.java
@@ -1,12 +1,18 @@
 package com.example.yangyistarter.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.math.BigInteger;
 
 @Data
 @Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "user")
 public class User {
     @Id
