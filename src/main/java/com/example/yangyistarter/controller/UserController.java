28aa3f7519a20e41355515753ed4ceab81b069a6
yangyi.shi <syy13983906480@163.com>
2021-08-30 10:15:00 +0800

    [ShiYangyi] add register interface in UserController

---
+++ src/main/java/com/example/yangyistarter/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.example.yangyistarter.controller;
+
+import com.example.yangyistarter.entity.User;
+import com.example.yangyistarter.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/users")
+public class UserController {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Autowired
+    UserService userService;
+
+    @PostMapping("/register")
+    public void register(@RequestBody User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        userService.register(user);
+    }
+}
