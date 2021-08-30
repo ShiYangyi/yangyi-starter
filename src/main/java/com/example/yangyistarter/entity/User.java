a56b08b7ae91a7a0fec27ea5726b072608eae5b1
yangyi.shi <syy13983906480@163.com>
2021-08-30 10:25:48 +0800

    [ShiYangyi] add Table and Column annotations

--- src/main/java/com/example/yangyistarter/entity/User.java
+++ src/main/java/com/example/yangyistarter/entity/User.java
@@ -2,17 +2,17 @@ package com.example.yangyistarter.entity;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.math.BigInteger;
 
 @Data
 @Entity
+@Table(name = "user")
 public class User {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private BigInteger id;
+    @Column(name = "username")
     private String name;
     private String password;
 }
