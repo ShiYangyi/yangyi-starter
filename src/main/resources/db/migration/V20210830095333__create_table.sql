af7778849f983a9bcbe8db54981b2e2b6e568a6a
yangyi.shi <syy13983906480@163.com>
2021-08-30 18:07:57 +0800

    [ShiYangyi] add create table sql

--- src/main/resources/db/migration/V20210830095333__create_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS `user` (
-    `id` bigint NOT NULL AUTO_INCREMENT,
-    `username` varchar(45) NOT NULL DEFAULT 1,
-    `password` longtext NOT NULL,
-    CONSTRAINT `USER_PK` PRIMARY KEY (`id`)
-);
