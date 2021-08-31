687ef611dbe2a58edb86c77fbd010fdb99b624d8
yangyi.shi <syy13983906480@163.com>
2021-08-31 11:25:56 +0800

    [ShiYangyi] reduce annotations range and simplify inner methods to increase test rate

--- src/main/java/com/example/yangyistarter/entity/User.java
+++ src/main/java/com/example/yangyistarter/entity/User.java
@@ -1,18 +1,19 @@
 package com.example.yangyistarter.entity;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.math.BigInteger;
 
-@Data
-@Entity
-@Builder
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
+//为什么把注解delombok后，这个注解注释掉还是会出错？不是已经产生了等价的方法了吗？？？？？？？？？？？？
+@Entity
 @Table(name = "user")
 public class User {
     @Id
@@ -21,4 +22,40 @@ public class User {
     @Column(name = "username")
     private String name;
     private String password;
+
+    public static UserBuilder builder() {
+        return new UserBuilder();
+    }
+
+    public static class UserBuilder {
+        private BigInteger id;
+        private String name;
+        private String password;
+
+        UserBuilder() {
+        }
+
+        /*public UserBuilder id(BigInteger id) {
+            this.id = id;
+            return this;
+        }*/
+
+        public UserBuilder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public UserBuilder password(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public User build() {
+            return new User(id, name, password);
+        }
+
+        /*public String toString() {
+            return "User.UserBuilder(id=" + this.id + ", name=" + this.name + ", password=" + this.password + ")";
+        }*/
+    }
 }
