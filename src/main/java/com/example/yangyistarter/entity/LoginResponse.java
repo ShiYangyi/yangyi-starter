3314d97d28382308861fdda01b01ca4494ed2a95
yangyi.shi <syy13983906480@163.com>
2021-09-02 11:06:30 +0800

    [ShiYangyi] delete unused annotations in LoginResponse

--- src/main/java/com/example/yangyistarter/entity/LoginResponse.java
+++ src/main/java/com/example/yangyistarter/entity/LoginResponse.java
@@ -1,14 +1,10 @@
 package com.example.yangyistarter.entity;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class LoginResponse {
     private String token;
     private User user;
