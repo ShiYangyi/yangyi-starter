882c7661c8ca542b00f332b1996acdc07fdce0f0
yangyi.shi <syy13983906480@163.com>
2021-09-03 11:36:23 +0800

    [ShiYangyi] delete unused annotations in UserDTO

--- src/main/java/com/example/yangyistarter/dto/UserDTO.java
+++ src/main/java/com/example/yangyistarter/dto/UserDTO.java
@@ -5,10 +5,6 @@ import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.validation.constraints.Pattern;
 import java.math.BigInteger;
 
@@ -17,10 +13,7 @@ import java.math.BigInteger;
 @Getter
 public class UserDTO {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private BigInteger id;
-    @Column(name = "username")
     @Length(max = 20, message = "用户名不能超过20个字符")
     @Length(min = 3, message = "用户名不能少于3个字符")
     @Pattern(regexp = "^[A-Za-z0-9\\-\\_]*$", message = "用户名只能包含字母、数字、-、_")
