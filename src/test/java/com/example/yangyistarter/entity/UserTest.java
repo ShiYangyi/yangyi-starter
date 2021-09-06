3d560a8d5796679cab756526013c963475d1ae99
yangyi.shi <syy13983906480@163.com>
2021-09-06 15:28:51 +0800

    [ShiYangyi] add test methods in UserTest

--- src/test/java/com/example/yangyistarter/entity/UserTest.java
+++ src/test/java/com/example/yangyistarter/entity/UserTest.java
@@ -13,6 +13,21 @@ class UserTest {
         User user = User.builder().name("小明").password("1").build();
         user.setId(BigInteger.valueOf(2L));
         Assertions.assertEquals(BigInteger.valueOf(2L), user.getId());
+    }
 
+    @Test
+    public void should_return_name_syyy_when_set_name_syyy() {
+
+        User user = User.builder().id((BigInteger.valueOf(2222L))).password("11111111").build();
+        user.setName("syyy");
+        Assertions.assertEquals("syyy", user.getName());
+    }
+
+    @Test
+    public void should_return_password_222222_when_set_password_222222() {
+
+        User user = User.builder().id((BigInteger.valueOf(2222L))).name("小明").build();
+        user.setPassword("222222");
+        Assertions.assertEquals("222222", user.getPassword());
     }
 }
