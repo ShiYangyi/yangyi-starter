9d01586ccb89d6e67c66e7fd0ac01a96964da92a
yangyi.shi <syy13983906480@163.com>
2021-09-10 10:40:29 +0800

    modify User to UserDTO in LoginResponse

--- src/main/java/com/example/yangyistarter/entity/LoginResponse.java
+++ src/main/java/com/example/yangyistarter/entity/LoginResponse.java
@@ -1,5 +1,6 @@
 package com.example.yangyistarter.entity;
 
+import com.example.yangyistarter.dto.UserDTO;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,13 +8,14 @@ import lombok.Setter;
 @Setter
 public class LoginResponse {
     private String token;
-    private User user;
+    //private User user;
+    private UserDTO user;
     private String message;
 
     public LoginResponse() {
     }
 
-    public LoginResponse(User user) {
+    public LoginResponse(UserDTO user) {
         this.user = user;
     }
 }
