b7690d697618139dbe88d2530de567a9d567295c
yangyi.shi <syy13983906480@163.com>
2021-09-14 17:03:58 +0800

    [Shi Yangyi] add notes in UserRepository

--- src/main/java/com/example/yangyistarter/repository/UserRepository.java
+++ src/main/java/com/example/yangyistarter/repository/UserRepository.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Repository;
 import java.math.BigInteger;
 
 @Repository
+//在接口上加上mapper注解，让mybatis底层来创建接口的实现类对象，这样就可以直接调用它的方法，
+// 自定义的repository类也是通过继承JpaRepository后，直接去使用父类中的方法，感觉是类似的作用，也都是通过调用底层的代码来操作数据库
 public interface UserRepository extends JpaRepository<User, BigInteger> {
 
 }
