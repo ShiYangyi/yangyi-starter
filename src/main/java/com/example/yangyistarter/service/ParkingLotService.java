7f46f5295e6784791d3acf8d2e6f3590372a1b99
yangyi.shi <syy13983906480@163.com>
2021-09-15 18:45:36 +0800

    [Shi Yangyi] add addParkingLot() in ParkingLotService

---
+++ src/main/java/com/example/yangyistarter/service/ParkingLotService.java
@@ -0,0 +1,34 @@
+package com.example.yangyistarter.service;
+
+import com.example.yangyistarter.entity.ParkingLot;
+import com.example.yangyistarter.repository.ParkingLotRepository;
+import com.example.yangyistarter.util.ResponseCode;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ParkingLotService {
+
+    ParkingLotRepository parkingLotRepository;
+    UserService userService;
+
+    public ParkingLotService(ParkingLotRepository parkingLotRepository, UserService userService) {
+        this.parkingLotRepository = parkingLotRepository;
+        this.userService = userService;
+    }
+
+    public ResponseCode addParkingLot(ParkingLot parkingLot) {
+        for (ParkingLot curParkingLot : parkingLotRepository.findAll()) {
+            if (curParkingLot.getName().equals(parkingLot.getName())) {
+                return ResponseCode.PARKINGLOT_ALREADY_EXISTS;
+            }
+        }
+        parkingLotRepository.save(parkingLot);
+        return ResponseCode.PARKINGLOT_ADD_SUCCESS;
+    }
+
+
+
+
+
+
+}
