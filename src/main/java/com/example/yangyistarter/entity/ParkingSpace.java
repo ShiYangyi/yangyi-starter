5a6454828b8791a05c7c9d47fba89e722ae4024e
yangyi.shi <syy13983906480@163.com>
2021-09-15 18:41:23 +0800

    [Shi Yangyi] add ParkingSpace

---
+++ src/main/java/com/example/yangyistarter/entity/ParkingSpace.java
@@ -0,0 +1,18 @@
+package com.example.yangyistarter.entity;
+
+import javax.persistence.*;
+import java.math.BigInteger;
+
+@Table(name = "parking_space")
+public class ParkingSpace {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private BigInteger id;
+    @Column(name = "isUsed")
+    private Boolean isUsed;
+    @Column(name = "receipt")
+    private String receipt;
+    @Column(name = "parkingLotName")
+    private BigInteger parkingLotName;
+
+}
