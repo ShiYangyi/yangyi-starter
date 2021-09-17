ea6f342ab2f5f64426b2aa5ded1c20d95d1f0cba
ShiYangyi <49311785+ShiYangyi@users.noreply.github.com>
2021-09-17 10:05:23 +0800

    Add user (#33)

---
+++ src/main/java/com/example/yangyistarter/controller/ManagerController.java
@@ -0,0 +1,33 @@
+package com.example.yangyistarter.controller;
+
+import com.example.yangyistarter.entity.User;
+import com.example.yangyistarter.service.ManagerService;
+import com.example.yangyistarter.service.UserService;
+import com.example.yangyistarter.util.ResponseCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/manager")
+public class ManagerController {
+    @Autowired
+    ManagerService managerService;
+
+    @PreAuthorize("hasRole('ROLE_MANAGER')")
+    @PostMapping("/add")
+    //测试这个接口时需要带上token，否则是无法验证用户身份的
+    //这里用到了aop，用到了代理，代理是框架来实现的，因为通过postman传进来的是一个json，
+    // json需要转换成对象，这些数据流的转换工作都是框架去实现的，在实现自己写的方法之前，
+    // 框架做了这些工作，当在方法中打了断点，在执行到return语句时，再单步调试就会进入到框架的代理方法。
+    // 代理方法相当于是在自己的实现方法外面包裹了一层。
+    public ResponseCode addUser(@RequestBody User user) {
+
+        return managerService.addUser(user);
+    }
+}
---
+++ src/test/java/com/example/yangyistarter/service/ManagerServiceTest.java
@@ -0,0 +1,96 @@
+package com.example.yangyistarter.service;
+
+import com.example.yangyistarter.entity.User;
+import com.example.yangyistarter.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(SpringExtension.class)
+public class ManagerServiceTest {
+    User user = mock(User.class);
+    User curUser = mock(User.class);
+    UserService userService = mock(UserService.class);
+    UserRepository userRepository = mock(UserRepository.class);
+    ManagerService managerService = new ManagerService(userService, userRepository);
+    /*User manager = User.builder().id(BigInteger.valueOf(1111L)).name("yyyy").password("yyyyyy").role("ROLE_MANAGER").build();
+    User cleverAssistant = User.builder().id(BigInteger.valueOf(1112L)).name("yyyX").password("yyyyyy").role("ROLE_CLEVER_ASSISTANT").build();
+    User stupidAssistant = User.builder().id(BigInteger.valueOf(1112L)).name("yyyX").password("yyyyyy").role("ROLE_STUPID_ASSISTANT").build();
+    User user = User.builder().id(BigInteger.valueOf(1113L)).name("yyyY").password("yyyyyy").role("ROLE_USER").build();*/
+
+    @Test
+    //会自动补齐前缀ROLE_
+    @WithMockUser(roles = {"MANAGER"})
+    public void should_add_user_when_role_is_manager() {
+
+        when(user.getRole()).thenReturn("ROLE_MANAGER");
+        when(user.getName()).thenReturn("syyy");
+        //when(curUser.getName()).thenReturn("yyyy");
+        when(userRepository.save(user)).thenReturn(user);
+
+        //写这个方法的测试应该不需要去管verifyDuplicate()方法的内部实现对吗，为什么还会报错呢，说是managerRepository为空，定位到findAll()。
+        //下面这条语句，managerService并不是mock的对象，是通过new出来的，所以下面语句的写法是错的，不是mock的对象并不能写成when().thenReturn()。
+        //但是managerService又必须不能是mock的对象，因为managerService.addUser(user)是需要测试的方法
+        //when(managerService.verifyDuplicate(user)).thenReturn(null);
+        //when(userRepository.findAll()).thenReturn(Collections.singletonList(curUser));
+        when(userService.findUserByName(user.getUsername())).thenReturn(null);
+        Assertions.assertEquals("user registered successful", managerService.addUser(user).getMessage());
+
+    }
+
+    @Test
+    //会自动补齐前缀ROLE_
+    @WithMockUser(roles = {"MANAGER"})
+    public void should_add_user_when_role_is_clever_assistant() {
+        when(user.getRole()).thenReturn("ROLE_CLEVER_ASSISTANT");
+        when(user.getName()).thenReturn("syyy");
+        //when(curUser.getName()).thenReturn("yyyy");
+        when(userRepository.save(user)).thenReturn(user);
+        //when(userRepository.findAll()).thenReturn(Collections.singletonList(curUser));
+        when(userService.findUserByName(user.getUsername())).thenReturn(null);
+        Assertions.assertEquals("user registered successful", managerService.addUser(user).getMessage());
+
+    }
+
+    @Test
+    //会自动补齐前缀ROLE_
+    @WithMockUser(roles = {"MANAGER"})
+    public void should_add_user_when_role_is_stupid_assistant() {
+        when(user.getRole()).thenReturn("ROLE_STUPID_ASSISTANT");
+        when(user.getName()).thenReturn("syyy");
+        //when(curUser.getName()).thenReturn("yyyy");
+        when(userRepository.save(user)).thenReturn(user);
+        //when(userRepository.findAll()).thenReturn(Collections.singletonList(curUser));
+        when(userService.findUserByName(user.getUsername())).thenReturn(null);
+        Assertions.assertEquals("user registered successful", managerService.addUser(user).getMessage());
+    }
+
+    @Test
+    //会自动补齐前缀ROLE_
+    @WithMockUser(roles = {"MANAGER"})
+    public void should_add_user_when_role_is_user() {
+        when(user.getRole()).thenReturn("ROLE_USER");
+        when(user.getName()).thenReturn("syyy");
+        //when(curUser.getName()).thenReturn("yyyy");
+        when(userRepository.save(user)).thenReturn(user);
+        //when(userRepository.findAll()).thenReturn(Collections.singletonList(curUser));
+        when(userService.findUserByName(user.getUsername())).thenReturn(null);
+        Assertions.assertEquals("invalid request", managerService.addUser(user).getMessage());
+    }
+
+    @Test
+    //会自动补齐前缀ROLE_
+    @WithMockUser(roles = {"MANAGER"})
+    public void should_add_user_failed_when_role_is_duplicate() {
+        when(user.getRole()).thenReturn("ROLE_STUPID_ASSISTANT");
+        when(user.getName()).thenReturn("syyy");
+        when(userRepository.save(user)).thenReturn(user);
+        //when(userRepository.findAll()).thenReturn(Collections.singletonList(user));
+        when(userService.findUserByName(user.getUsername())).thenReturn(user);
+        Assertions.assertEquals("user already exists", managerService.addUser(user).getMessage());
+    }
+
+}
