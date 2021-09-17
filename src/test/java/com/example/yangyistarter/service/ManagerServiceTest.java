724c3efa0024c3d26cc1c76a53f2cfbad5a374f2
yangyi.shi <syy13983906480@163.com>
2021-09-17 09:48:45 +0800

    [Shi Yangyi] modify test methods with findUserByName() in Service layer

--- src/test/java/com/example/yangyistarter/service/ManagerServiceTest.java
+++ src/test/java/com/example/yangyistarter/service/ManagerServiceTest.java
@@ -7,15 +7,15 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import java.util.Collections;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(SpringExtension.class)
 public class ManagerServiceTest {
     User user = mock(User.class);
     User curUser = mock(User.class);
+    UserService userService = mock(UserService.class);
     UserRepository userRepository = mock(UserRepository.class);
-    ManagerService managerService = new ManagerService(userRepository);
+    ManagerService managerService = new ManagerService(userService, userRepository);
     /*User manager = User.builder().id(BigInteger.valueOf(1111L)).name("yyyy").password("yyyyyy").role("ROLE_MANAGER").build();
     User cleverAssistant = User.builder().id(BigInteger.valueOf(1112L)).name("yyyX").password("yyyyyy").role("ROLE_CLEVER_ASSISTANT").build();
     User stupidAssistant = User.builder().id(BigInteger.valueOf(1112L)).name("yyyX").password("yyyyyy").role("ROLE_STUPID_ASSISTANT").build();
@@ -28,14 +28,15 @@ public class ManagerServiceTest {
 
         when(user.getRole()).thenReturn("ROLE_MANAGER");
         when(user.getName()).thenReturn("syyy");
-        when(curUser.getName()).thenReturn("yyyy");
+        //when(curUser.getName()).thenReturn("yyyy");
         when(userRepository.save(user)).thenReturn(user);
 
         //写这个方法的测试应该不需要去管verifyDuplicate()方法的内部实现对吗，为什么还会报错呢，说是managerRepository为空，定位到findAll()。
         //下面这条语句，managerService并不是mock的对象，是通过new出来的，所以下面语句的写法是错的，不是mock的对象并不能写成when().thenReturn()。
         //但是managerService又必须不能是mock的对象，因为managerService.addUser(user)是需要测试的方法
         //when(managerService.verifyDuplicate(user)).thenReturn(null);
-        when(userRepository.findAll()).thenReturn(Collections.singletonList(curUser));
+        //when(userRepository.findAll()).thenReturn(Collections.singletonList(curUser));
+        when(userService.findUserByName(user.getUsername())).thenReturn(null);
         Assertions.assertEquals("user registered successful", managerService.addUser(user).getMessage());
 
     }
@@ -46,9 +47,10 @@ public class ManagerServiceTest {
     public void should_add_user_when_role_is_clever_assistant() {
         when(user.getRole()).thenReturn("ROLE_CLEVER_ASSISTANT");
         when(user.getName()).thenReturn("syyy");
-        when(curUser.getName()).thenReturn("yyyy");
+        //when(curUser.getName()).thenReturn("yyyy");
         when(userRepository.save(user)).thenReturn(user);
-        when(userRepository.findAll()).thenReturn(Collections.singletonList(curUser));
+        //when(userRepository.findAll()).thenReturn(Collections.singletonList(curUser));
+        when(userService.findUserByName(user.getUsername())).thenReturn(null);
         Assertions.assertEquals("user registered successful", managerService.addUser(user).getMessage());
 
     }
@@ -59,9 +61,10 @@ public class ManagerServiceTest {
     public void should_add_user_when_role_is_stupid_assistant() {
         when(user.getRole()).thenReturn("ROLE_STUPID_ASSISTANT");
         when(user.getName()).thenReturn("syyy");
-        when(curUser.getName()).thenReturn("yyyy");
+        //when(curUser.getName()).thenReturn("yyyy");
         when(userRepository.save(user)).thenReturn(user);
-        when(userRepository.findAll()).thenReturn(Collections.singletonList(curUser));
+        //when(userRepository.findAll()).thenReturn(Collections.singletonList(curUser));
+        when(userService.findUserByName(user.getUsername())).thenReturn(null);
         Assertions.assertEquals("user registered successful", managerService.addUser(user).getMessage());
     }
 
@@ -71,9 +74,10 @@ public class ManagerServiceTest {
     public void should_add_user_when_role_is_user() {
         when(user.getRole()).thenReturn("ROLE_USER");
         when(user.getName()).thenReturn("syyy");
-        when(curUser.getName()).thenReturn("yyyy");
+        //when(curUser.getName()).thenReturn("yyyy");
         when(userRepository.save(user)).thenReturn(user);
-        when(userRepository.findAll()).thenReturn(Collections.singletonList(curUser));
+        //when(userRepository.findAll()).thenReturn(Collections.singletonList(curUser));
+        when(userService.findUserByName(user.getUsername())).thenReturn(null);
         Assertions.assertEquals("invalid request", managerService.addUser(user).getMessage());
     }
 
@@ -84,7 +88,8 @@ public class ManagerServiceTest {
         when(user.getRole()).thenReturn("ROLE_STUPID_ASSISTANT");
         when(user.getName()).thenReturn("syyy");
         when(userRepository.save(user)).thenReturn(user);
-        when(userRepository.findAll()).thenReturn(Collections.singletonList(user));
+        //when(userRepository.findAll()).thenReturn(Collections.singletonList(user));
+        when(userService.findUserByName(user.getUsername())).thenReturn(user);
         Assertions.assertEquals("user already exists", managerService.addUser(user).getMessage());
     }
 
