cb43e5caaa94de8a79defd60c1e5a9a0e262935d
yangyi.shi <syy13983906480@163.com>
2021-09-22 13:44:40 +0800

    [Shi Yangyi] add parking interface

---
+++ src/main/java/com/example/yangyistarter/controller/ParkingController.java
@@ -0,0 +1,37 @@
+package com.example.yangyistarter.controller;
+
+import com.example.yangyistarter.service.ParkingService;
+import lombok.AllArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigInteger;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("parking")
+public class ParkingController {
+
+    ParkingService parkingService;
+
+    @PreAuthorize("hasRole('ROLE_MANAGER')")
+    @PostMapping("/managers")
+    public BigInteger randomParking() {
+        return parkingService.randomParking();
+    }
+
+    @PreAuthorize("hasRole('ROLE_CLEVER_ASSISTANT')")
+    @PostMapping("/clever/assistants")
+    public BigInteger cleverParking() {
+        return parkingService.cleverParking();
+    }
+
+    @PreAuthorize("hasAnyRole('ROLE_STUPID_ASSISTANT', 'ROLE_USER')")
+    @PostMapping("/users")
+    public BigInteger parking() {
+        return parkingService.parking();
+    }
+
+}
