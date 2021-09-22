6396a7e078388c0964e785a34d7df43cb20de690
yangyi.shi <syy13983906480@163.com>
2021-09-22 13:45:57 +0800

    [Shi Yangyi] add parking space entity

--- src/main/java/com/example/yangyistarter/entity/ParkingSpace.java
+++ src/main/java/com/example/yangyistarter/entity/ParkingSpace.java
@@ -1,19 +1,29 @@
 package com.example.yangyistarter.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import javax.persistence.*;
 import java.math.BigInteger;
 
 @Entity
 @Table(name = "parking_space")
+@Builder
+@Setter
+@Getter
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class ParkingSpace {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private BigInteger id;
-    @Column(name = "isUsed")
+    //在程序里面使用驼峰命令，在数据库中使用_以及小写
+    @Column(name = "is_used")
     private Boolean isUsed;
-    @Column(name = "receipt")
-    private String receipt;
-    @Column(name = "parkingLotName")
-    private BigInteger parkingLotName;
-
+    @Column(name = "receipt_id")
+    private BigInteger receiptId;
+    @Column(name = "parking_lot_name")
+    private String parkingLotName;
 }
