9ae92ff351f6c93e4fd2394d6b4290bdeb7ca605
yangyi.shi <syy13983906480@163.com>
2021-09-23 16:05:07 +0800

    [Shi Yangyi] add pick up car interface test in PickingServiceTest

---
+++ src/test/java/com/example/yangyistarter/service/PickingServiceTest.java
@@ -0,0 +1,53 @@
+package com.example.yangyistarter.service;
+
+import com.example.yangyistarter.entity.ParkingSpace;
+import com.example.yangyistarter.repository.ParkingSpaceRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.math.BigInteger;
+import java.util.Optional;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
+public class PickingServiceTest {
+
+    ParkingSpaceRepository parkingSpaceRepository = mock(ParkingSpaceRepository.class);
+    ParkingSpace parkingSpace = mock(ParkingSpace.class);
+    PickingService pickingService = new PickingService(parkingSpaceRepository);
+
+    @Test
+    public void should_return_response_when_receipt_id_invalid() {
+        BigInteger receiptId = BigInteger.ONE;
+        when(parkingSpaceRepository.findById(receiptId)).thenReturn(Optional.empty());
+        Assertions.assertEquals(10012, pickingService.picking(receiptId).getCode());
+        Assertions.assertEquals("receipt is invalid", pickingService.picking(receiptId).getMessage());
+
+    }
+
+    @Test
+    public void should_return_response_when_receipt_parking_space_free() {
+        BigInteger receiptId = BigInteger.ONE;
+        when(parkingSpaceRepository.findById(receiptId)).thenReturn(Optional.of(parkingSpace));
+        when(parkingSpace.getReceiptId()).thenReturn(BigInteger.ONE);
+        when(parkingSpace.getIsUsed()).thenReturn(false);
+        Assertions.assertEquals(10014, pickingService.picking(receiptId).getCode());
+        Assertions.assertEquals("free parking space and invalid request", pickingService.picking(receiptId).getMessage());
+
+    }
+
+    @Test
+    public void should_return_response_when_receipt_is_valid() {
+        BigInteger receiptId = BigInteger.ONE;
+        when(parkingSpaceRepository.findById(receiptId)).thenReturn(Optional.of(parkingSpace));
+        when(parkingSpace.getReceiptId()).thenReturn(BigInteger.ONE);
+        when(parkingSpace.getIsUsed()).thenReturn(true);
+        Assertions.assertEquals(10013, pickingService.picking(receiptId).getCode());
+        Assertions.assertEquals("pick up car successful", pickingService.picking(receiptId).getMessage());
+
+    }
+}
